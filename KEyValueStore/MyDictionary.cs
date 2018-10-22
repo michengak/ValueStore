911d3a14aabe55eb8850fdffec703f9efde29737
michel ngako <emoumichel@gmail.com>
2018-10-22 08:44:05 -0400

    Add project files.

---
+++ KEyValueStore/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEyValueStore
+{
+   
+        class MyDictionary
+        {
+            private KeyValue[] arr = new KeyValue[15];
+            private int counter = 0;
+
+            public object this[string key]
+            {
+               
+                set
+                {
+                    bool exists = true;
+                    for (int i = 0; i < counter; i++)
+                    {
+                        if (arr[i].key == key)
+                        {
+                            arr[i] = new KeyValue(key, value);
+                            exists = false;
+                        }
+                    }
+                    if (exists && counter != 15)
+                    {
+                        arr[counter++] = new KeyValue(key, value);
+                    }
+                    else
+                    {
+                        throw new Exception("You are out of the recommend number");
+                    }
+                }
+                get
+                {
+                    foreach (KeyValue x in arr)
+                    {
+                        if (x.key == key)
+                        {
+                            return x.value;
+                        }
+                    }
+                    throw new KeyNotFoundException();
+                }
+
+            }
+        }
+    
+
+}
+
---
+++ KEyValueStore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEyValueStore
+{
+    struct KeyValue
+    {
+        public KeyValue(string key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public readonly string key;
+        public readonly object value;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
+   
+
